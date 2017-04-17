4aa4f9a7c8e01d87c4c3f77434848ca341c16666
Daniil Pintjuk <pintjuk@kth.se>
2017-04-17 19:01:35 +0200

    type checking expressions

--- src/main/scala/punkt0/analyzer/TypeChecking.scala
+++ src/main/scala/punkt0/analyzer/TypeChecking.scala
@@ -11,8 +11,51 @@ object TypeChecking extends Phase[Program, Program] {
   /** Typechecking does not produce a value, but has the side effect of
    * attaching types to trees and potentially outputting error messages. */
   def run(prog: Program)(ctx: Context): Program = {
+
+    val globalScope = prog.getSymbol;
+
     def tcExpr(expr: ExprTree, expected: Type*): Type = {
-      val tpe: Type = ??? // TODO: Compute type for each kind of expression
+      val tpe: Type = expr match{
+        case v:And        =>  tcExpr(v.lhs, TBoolean); tcExpr(v.rhs, TBoolean);
+        case v:Or         =>  tcExpr(v.lhs, TBoolean); tcExpr(v.rhs, TBoolean);
+        case v:Plus       =>  val lt = tcExpr(v.lhs, TInt, TString); 
+                              val rt = tcExpr(rhs, TInt, TString); 
+                              if(rt==Tint && lt==TInt)  TInt 
+                              else                      TString;
+        case v:Minus      =>  tcExpr(v.lhs, TInt); tcExpr(v.rhs, TInt);
+        case v:Times      =>  tcExpr(v.lhs, TInt); tcExpr(v.rhs, TInt);
+        case v:Div        =>  tcExpr(v.lhs, TInt); tcExpr(v.rhs, TInt);
+        case v:LessThan   =>  tcExpr(v.lhs, TInt); tcExpr(v.rhs, TInt); TBoolean;
+        case v:Equals     =>  val lt = tcExpr(v.lhs);
+                              val rt = tcExpr(v.rhs);
+                              if(lt==rt) TBoolean
+                              else if(!(lt==rt) && lt.isInstanceOf[TClass]&& rt.isInstanceOf[TClass])
+                              else error("Type error: type missmatch, cant compare primatives and classes", expr); TError;
+        case v:MethodCall =>  tcExpr(v.obj, Types.anyRef).classSymbol.lookupMethod(v.meth) match {
+                                case None => error("Type error: object does not contain reference" + v.meth, v.obj); TError;
+                                case Some(methSymbol) v.args zip methSymbol.argList map {case (arg, param) => tcExpr(arg, param)}; methSymbol.retType;
+                              }
+        case v:IntLit     =>  TInt
+        case v:StringLit  =>  TString
+        case v:True       =>  TBoolean
+        case v:False      =>  TBoolean
+        case v:Identifier =>  v.getSymbol.getType
+        case v:This       =>  v.getSymbol.getType
+        case v:Null       =>  if(expected.isEmpty) Types.anyRef
+                              else if(expected.exists(e => e.isInstanceOf[TClass])) expected.filter(e => e.isInstanceOf[TClass]).first   
+                              else Report.error("Type error: this cant be Null", v); TError;
+        case v:New        =>  v.tpe.getSymbol.getType
+        case v:Not        =>  tcExpr(v.expr, TBoolean)
+        case v:Block      =>  v.exprs.slice(0, v.exprs.length-2).map(tcExpr(_)); tcExpr(v.exprs.last, expected);
+        case v:If         =>  tcExpr(v.expr, IBoolean);
+                              val thnT = tcExpr(v.thn, expected);
+                              v.els match{
+                                case None       => thnT;
+                                case Some(els)  => val elsT = tcExpr(els, expected);
+                              }
+      }
+
+      expr.setType(tpe);
 
 
       // Check result and return a valid type in case of error
@@ -25,8 +68,15 @@ object TypeChecking extends Phase[Program, Program] {
         tpe
       }
     }
+    
+    def tcMeth(mth: MethodDecl, expected: Type*): Type = {
+      mth.vars.map(vr => vr.setType = tcExpr(vr.expr, vr.getSymbol.getType))
+      mth.exprs.map(tcExpr);
+      tcExpr(mth.retExpr, expected);
+    }
 
-    val globalScope = prog.getSymbol;
+    
+
 
     def makeType(tpe : TypeTree): Type = {
       tpe match {
@@ -71,17 +121,21 @@ object TypeChecking extends Phase[Program, Program] {
         if(mth.overrides)
           if(mth.getSymbol.argList.length == mth.getSymbol.overridden.get.argList.length){
             if(!(mth.getSymbol.argList zip mth.getSymbol.overridden.get.argList map {case (left, right) => left.getType == right.getType} forall Predef.identity))
-              Reporter.error("argumets have to match when overriding", mth)
+              Reporter.error("Type error: argumets have to match when overriding", mth)
             if(mth.getSymbol.retType != mth.getSymbol.overridden.get.retType)
-              Reporter.error("return types must match when overriding", mth.retType)
+              Reporter.error("Type error: return types must match when overriding", mth.retType)
           }
           else{
-            Reporter.error("arguments have to match when overriding", mth)
+            Reporter.error("Type error: arguments have to match when overriding", mth)
           }
       })
     })
 
-    /**/
+    /* typecheck expressions add such */
+   prog.classes.map(cl => {
+     cl.vars.map(vr=> vr.setType(tcExpr(vr.expr, vr.getSymbol.getType)))
+     cl.methods.map(mth=> tcMeth(mth,mth.getSymbol.retType))
+   })
     prog
   }
 }
