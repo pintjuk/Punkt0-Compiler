b809d179c849b3e96587034dfa4a31ce496a05f7
Daniil Pintjuk <pintjuk@kth.se>
2017-04-27 14:57:44 +0200

    working on code generation

---
+++ experements/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        
+        for(int i=0; i<2;i++){
+            boolean hej = i<i;
+        }
+    }
+
+}
--- src/main/scala/punkt0/code/CodeGeneration.scala
+++ src/main/scala/punkt0/code/CodeGeneration.scala
@@ -26,7 +26,7 @@ object CodeGeneration extends Phase[Program, Unit] {
       val classFile = new ClassFile(ct.id.value, ct.parent map (x => x.value))
       classFile.setSourceFile(ct.file.getPath)
       
-      ct.vars map (x => val fn = classFile.addField( makeType(x.tpe), x.id.value))
+      ct.vars map (x =>  classFile.addField( makeType(x.tpe), x.id.value));
       ct.methods map ( mth => 
           generateMethodCode( 
             classFile.addMethod(
@@ -37,7 +37,8 @@ object CodeGeneration extends Phase[Program, Unit] {
             mth 
           ) 
         )
-      classFile.writeToFile(dir + "/" + ct.id.value + ".class");
+    
+      classFile.writeToFile(dir + "/" + ct.id.value + ".class" );
     }
 
     // a mapping from variable symbols to positions in the local variables
@@ -74,11 +75,43 @@ object CodeGeneration extends Phase[Program, Unit] {
         case v:And        => 
         case v:Or         =>
         case v:Plus       =>
-        case v:Minus      => 
-        case v:Times      => 
-        case v:Div        => 
-        case v:LessThan   => 
-        case v:Equals     => 
+        case v:Minus      =>  generateExpr(ch, v.lhs, slotFor):::generateExpr(ch, v.rhs, slotFor):::List(ISUB)  
+        case v:Times      =>  generateExpr(ch, v.lhs, slotFor):::generateExpr(ch, v.rhs, slotFor):::List(IMUL)  
+        case v:Div        =>  generateExpr(ch, v.lhs, slotFor):::generateExpr(ch, v.rhs, slotFor):::List(IDIV)  
+        case v:LessThan   =>{
+                              val lTrue = ch.getFreshLabel()
+                              val lEnd = ch.getFreshLabel();
+                              generateExpr(ch, v.lhs, slotFor):::
+                              generateExpr(ch, v.rhs, slotFor):::
+                              If_ICmpLt(lTrue):::
+                                ICONST_0:::
+                                Goto(lEnd):::
+                              Lable(lTrue):::
+                                ICONST_1:::
+                              Lable(lEnd);
+                            }
+        case v:Equals     => if(v.lhs.getType==TInt && v.rhs.getType==TInt)  
+                            {
+                             
+                              val lTrue = ch.getFreshLabel()
+                              val lEnd = ch.getFreshLabel();
+                              generateExpr(ch, v.lhs, slotFor):::
+                              generateExpr(ch, v.rhs, slotFor):::
+                              List(ISUB):::
+                              IfEq(lTrue):::
+                                ICONST_0:::
+                                Goto(lEnd):::
+                              Lable(lTrue):::
+                                ICONST_1:::
+                              Lable(lEnd);
+                            }else if (v.lhs.getType==TBoolean && v.rhs.getType==TBoolean) {
+                              //TODO boolean lazy compare 
+                            }else if ((v.lhs.getType==TString && v.rhs.getType==TString) || 
+                                      (v.lhs.getType.isInstanceOf (TClass) && v.rhs.getType.isInstanceOf(TClass))) {
+                                        //TODO: compare by reference
+                            }else{
+                              throw new RuntimeException("WTF? this sholud not happen, types are mismatched,\n the type checker should have caught this")
+                            }
         case v:MethodCall =>  
         case v:IntLit     => Ldc(v.value)
         case v:StringLit  => Ldc(v.vlaue)
@@ -88,7 +121,7 @@ object CodeGeneration extends Phase[Program, Unit] {
         case v:This       => 
         case v:Null       => ACONST_NULL
         case v:New        => DefaultNew("L"+v.tpe.value+";")
-        case v:Not        => INEG 
+        case v:Not        => generateExpr(ch,v.expr,slotFor):::List(INEG)
         case v:Block      => (v.exprs map  generateExpr(ch, _, slotFor)) foldLeft (Nil) (_:::_)
         case v:If         => 
         case v:While      =>  
