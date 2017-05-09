aabad8b4d25c2bb4748262188543da22dc3c36f6
Daniil Pintjuk <pintjuk@kth.se>
2017-05-09 14:48:20 +0200

    added tests, fixed type checking bug, updated reference compiler

--- makeOurcheck.sh
+++ makeOurcheck.sh
@@ -3,7 +3,7 @@ rm -rf testprograms/lab5/check
 mkdir testprograms/lab5/check
 for i in $( ls testprograms/our_valid/*.p0); do
     echo item: $i
-    scala -cp lib/cafebabe_2.11-1.2.jar punkt0_2.11-1.0.jar -d dest $i
+    scala -cp lib/cafebabe_2.11-1.2.jar punkt0_2.11-1.1.jar -d dest $i
     java -cp dest Main > $i.check 
     cat $i.check
 done
